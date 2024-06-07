521e2461abc253c1140cdafc3e5c72e830ab5bea
Bruno Alves <nito.ba.dev@gmail.com>
2024-06-07 01:59:22 -0300

    fix: upload to folder correct and add google docs format

--- src/repositories/google_drive_repository.py
+++ src/repositories/google_drive_repository.py
@@ -10,27 +10,33 @@ from googleapiclient.http import MediaIoBaseUpload
 SERVICE_ACCOUNT_FILE = f'{path.dirname(__file__)}/../../credentials.json'
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
-credentials = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES
-)
 
 
 class GoogleDriveRepository(IUploader):
     def __init__(self) -> None:
+        credentials = service_account.Credentials.from_service_account_file(
+            SERVICE_ACCOUNT_FILE, scopes=SCOPES
+        )
         self.client = build('drive', 'v3', credentials=credentials)
 
     def upload(self, content: str, filename: str):
-        file_metadata = {'name': filename}
-        file_metadata['parents'] = env.DRIVE_FOLDER_ID
+        file_metadata = {
+            'name': filename,
+            'parents': [env.DRIVE_FOLDER_ID],
+            'mimeType': 'application/vnd.google-apps.document',
+        }
 
         content_io = io.BytesIO(content.encode('utf-8'))
 
         media = MediaIoBaseUpload(content_io, mimetype='text/plain', resumable=True)
-        file = (
-            self.client.files()
-            .create(body=file_metadata, media_body=media, fields='id')
-            .execute()
-        )
-        print(
-            f"Arquivo '{filename}' foi enviado com sucesso para o Google Drive. ID do arquivo: {file.get('id')}"
-        )
+        try:
+            file = (
+                self.client.files()
+                .create(body=file_metadata, media_body=media, fields='id')
+                .execute()
+            )
+            print(
+                f"Arquivo '{filename}' foi enviado com sucesso para o Google Drive. ID do arquivo: {file.get('id')}"
+            )
+        except Exception as err:
+            print(err)
