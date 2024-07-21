07592cdf9176245da1e64638f8b49f739eb56c7a
Bruno Alves <nito.ba.dev@gmail.com>
2024-07-21 01:51:43 -0300

    feat: get google credentials from env and setup dockerfile

--- server/src/repositories/google_drive_repository.py
+++ server/src/repositories/google_drive_repository.py
@@ -15,8 +15,20 @@ SCOPES = ['https://www.googleapis.com/auth/drive']
 
 class GoogleDriveRepository(IUploader):
     def __init__(self) -> None:
-        credentials = service_account.Credentials.from_service_account_file(
-            SERVICE_ACCOUNT_FILE, scopes=SCOPES
+        service_account_info = {
+            'type': env.GOOGLE_SERVICE_ACCOUNT_TYPE,
+            'project_id': env.GOOGLE_PROJECT_ID,
+            'private_key_id': env.GOOGLE_PRIVATE_KEY_ID,
+            'private_key': env.GOOGLE_PRIVATE_KEY.replace('\\n', '\n'),
+            'client_email': env.GOOGLE_CLIENT_EMAIL,
+            'client_id': env.GOOGLE_CLIENT_ID,
+            'auth_uri': env.GOOGLE_AUTH_URI,
+            'token_uri': env.GOOGLE_TOKEN_URI,
+            'auth_provider_x509_cert_url': env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+            'client_x509_cert_url': env.GOOGLE_CLIENT_X509_CERT_URL,
+        }
+        credentials = service_account.Credentials.from_service_account_info(
+            service_account_info, scopes=SCOPES
         )
         self.client = build('drive', 'v3', credentials=credentials)
 
