6eadee566278966af28548399005b9145c968b6c
Bruno Alves <nito.ba.dev@gmail.com>
2024-07-28 02:52:17 -0300

    chore: copyright text

--- web/src/app/(auth)/layout.tsx
+++ web/src/app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { Stars } from 'lucide-react'
 export default function AuthLayout({
   children,
 }: Readonly<{
@@ -26,29 +27,18 @@ export default function AuthLayout({
       <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Acme Inc
+          <div className="relative z-20 flex items-center text-lg font-medium font-mono">
+            <Stars className="h-6 w-6 mr-2" />
+            Report Creator AI
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
+                &ldquo;Save time and productivity. Leave it to us to write your
+                weekly report and focus on what is most important, delivering
+                your results.&rdquo;
               </p>
-              <footer className="text-sm">Sofia Davis</footer>
+              <footer className="text-sm">Report Creator AI</footer>
             </blockquote>
           </div>
         </div>
